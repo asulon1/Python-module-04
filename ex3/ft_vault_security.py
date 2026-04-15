a5f05d4f384af685b57e66764eaa476b179e1798
asulon <adrien.sulon@epitech.eu>
2026-04-15 17:58:00 +0200

    ex3 update with new subject

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -6,29 +6,47 @@
 #  By: asulon <asulon@student.42nice.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 0026/03/08 00:24:33 by sulon           #+#    #+#               #
-#  Updated: 2026/03/09 19:58:54 by asulon          ###   ########.fr        #
+#  Updated: 2026/04/15 17:57:43 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from typing import Optional
 
-print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
-print("Initiating secure vault access...")
 
-try:
-    print("Vault connection established with failsafe protocols\n")
+def secure_archive(file_name: str, action: Optional[str] = 'r',
+                   new_content: Optional[str] = ""
+                   ) -> tuple[bool, str]:
+    """ return True and content if action successed
+        otherwise False and an Error message"""
 
-    with open("classified_data.txt", "r") as vault_file:
-        data = vault_file.read()
-        print("SECURE EXTRACTION:")
-        print(f"{data}\n")
+    # Write in file
+    if (action == "write" or action == "w") and new_content is not None:
+        try:
+            with open(file_name, "w") as file:
+                file.write(new_content)
+                return (True, new_content)
+        except (FileNotFoundError, PermissionError) as error:
+            return (False, f"{error}")
+    else:
+        # Read file
+        try:
+            with open(file_name) as file:
+                return (True, file.read())
+        except (FileNotFoundError, PermissionError) as error:
+            return (False, f"{error}")
 
-    with open("classified_vault.txt", "w") as archive_file:
-        archive_file.write("[CLASSIFIED] New security protocols archived")
-        print("SECURE PRESERVATION:")
-        print("[CLASSIFIED] New security protocols archived")
 
-    print("Vault automatically sealed upon completion\n")
-except FileNotFoundError:
-    print("Error: Vault source not found")
-finally:
-    print("All vault operations completed with maximum security.")
+print("=== Cyber Archives Security ===\n")
+
+print("Using 'secure_archive' to read from a nonexistent file:")
+print(f"{secure_archive("/not/existing/file")}\n")
+
+print("Using 'secure_archive' to read from an inaccessible file:")
+print(f"{secure_archive("/etc/master.passwd")}\n")
+
+print("Using 'secure_archive' to read from a regular file:")
+print(f"{secure_archive("ancient_fragment.txt")}\n")
+
+print("Using 'secure_archive' to write previous content to a new file:")
+print(f"{secure_archive("ancient_fragment", "write",
+                        'Content successfully written to file')}\n")
